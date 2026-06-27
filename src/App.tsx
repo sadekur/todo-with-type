acb30bcf5b099fc37dd8adba73b90c36d3faacd8
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-27 20:42:36 +0600

    Add: Pass typo props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import React from 'react';
+import Todos from './components/Todos';
+
+const todoData = [
+  {
+    id: 1,
+    title: "Learn React",
+  },
+  {
+    id: 2,
+    title: "Learn Vue",
+  },
+  {
+    id: 3,
+    title: "Learn Angular",
+  }
+]
+
 function App() {
   return (
-   <div>Hello World</div>
+   <div><Todos todos={todoData} /></div>
   );
 }
 
