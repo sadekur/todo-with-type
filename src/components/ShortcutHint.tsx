cbcd294d63f59e705f2f79a9c96dd7f3451469fe
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-27 22:47:31 +0600

    docs: add Esc shortcut hint to ShortcutHint component

--- src/components/ShortcutHint.tsx
+++ src/components/ShortcutHint.tsx
@@ -4,7 +4,8 @@ const ShortcutHint = () => (
   <div className="mb-4 text-center text-xs text-gray-400 dark:text-gray-500">
     <kbd className="rounded border border-gray-300 px-1 dark:border-gray-600">N</kbd> add &middot;
     <kbd className="ml-1 rounded border border-gray-300 px-1 dark:border-gray-600">/</kbd> search &middot;
-    <kbd className="ml-1 rounded border border-gray-300 px-1 dark:border-gray-600">?</kbd> help
+    <kbd className="ml-1 rounded border border-gray-300 px-1 dark:border-gray-600">?</kbd> help &middot;
+    <kbd className="ml-1 rounded border border-gray-300 px-1 dark:border-gray-600">Esc</kbd> close
   </div>
 );
 
