8ab107da19f98d0543ca0fc23b22f2db7c382863
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-27 20:39:04 +0600

    Add Tailwind css packahe

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 
+const todoData = [
+  {
+    id: 1,
+    title: "Learn React",
+  },
+  {
+    id: 2,
+    title: "Learn Vue",
+  },
+  {
+    id: 3,
+    title: "Learn Angular",
+  }
+]
+
 function App() {
   return (
    <div>Hello World</div>
